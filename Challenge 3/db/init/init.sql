b41d9668750e2acb2786c57bfb9ddcaa39a09deb
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2024-04-24 06:59:20 -0600

    Add files via upload

---
+++ Challenge 3/db/init/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
+
+INSERT INTO books (id, title, author) VALUES (1, 'Anne of Green Gables', 'L.M> Montgomery');
+
+INSERT INTO books (id, title, author) VALUES (2, 'No Great Mischief', 'Alistair MacLeod');
+
+INSERT INTO books (id, title, author) VALUES (3, 'The Stone Angel', 'Margaret Laurence');
+
+INSERT INTO books (id, title, author) VALUES (4, 'Fifth Business', 'Robertson Davies');
+
